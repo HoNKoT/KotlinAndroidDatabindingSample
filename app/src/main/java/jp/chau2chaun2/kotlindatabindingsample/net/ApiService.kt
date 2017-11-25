55ffbee11852382022b0b294892393d1d8457e23
Hiroki Honda <hon87kot12.xp@gmail.com>
2017-11-25 01:07:41 -0800

    put some comments

--- app/src/main/java/jp/chau2chaun2/kotlindatabindingsample/net/ApiService.kt
+++ app/src/main/java/jp/chau2chaun2/kotlindatabindingsample/net/ApiService.kt
@@ -15,16 +15,33 @@ import java.util.concurrent.TimeUnit
 
 class ApiService {
 
+    // core for controller
     val service: IApiService = create(IApiService::class.java)
 
     lateinit var retrofit: Retrofit
 
+    fun <S> create(serviceClass: Class<S>): S {
+        val gson = GsonBuilder()
+                .serializeNulls()
+                .create()
+
+        // create retrofit
+        retrofit = Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .baseUrl("http://randomuser.me/")
+                .client(httpBuilder.build())
+                .build()
+
+        return retrofit.create(serviceClass)
+    }
+
     val httpBuilder: OkHttpClient.Builder get() {
+        // create http client
         val httpClient = OkHttpClient.Builder()
                 .addInterceptor(Interceptor { chain ->
                     val original = chain.request()
 
-                    //header設定
+                    //header
                     val request = original.newBuilder()
                             .header("Accept", "application/json")
                             .method(original.method(), original.body())
@@ -34,25 +51,12 @@ class ApiService {
                 })
                 .readTimeout(30, TimeUnit.SECONDS)
 
+        // log interceptor
         val loggingInterceptor = HttpLoggingInterceptor()
         loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
         httpClient.addInterceptor(loggingInterceptor)
 
         return httpClient
-    }
-
-    fun <S> create(serviceClass: Class<S>): S {
-        val gson = GsonBuilder()
-                .serializeNulls()
-                .create()
-
-        retrofit = Retrofit.Builder()
-                .addConverterFactory(GsonConverterFactory.create(gson))
-                .baseUrl("http://randomuser.me/")
-                .client(httpBuilder.build())
-                .build()
-
-        return retrofit.create(serviceClass)
     }
 }
 
